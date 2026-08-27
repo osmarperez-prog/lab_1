6bdab487fbf0b657ad79f9800cfe335e9f470641
johnsanga-web <john.sanga@ucb.edu.bo>
2026-08-27 18:56:35 -0400

    nombrador

---
+++ nombre_ejercicio.py
@@ -0,0 +1,4 @@
+nombre = input("Ingrese su nombre: ")
+edad = input("Ingrese su edad: ")
+profesion = input("Ingrese su profesión: ")
+print("Hola", nombre, "tienes", edad, "años y eres", profesion)
